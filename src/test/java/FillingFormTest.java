832a13c1bae6f5ed8f52dab4ed83101d29b206e3
Sergey <zendmir5@yandex.ru>
2021-02-07 19:22:52 +0300

    test refactoring

--- src/test/java/FillingFormTest.java
+++ src/test/java/FillingFormTest.java
@@ -1,9 +1,8 @@
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -13,13 +12,17 @@ public class FillingFormTest extends TestBase {
     SelenideElement firstNameField = $("#firstName");
     SelenideElement lastNameField = $("#lastName");
     SelenideElement emailField = $("#userEmail");
+    SelenideElement genderOptions = $("#genterWrapper");
     SelenideElement mobileNumberField = $("#userNumber");
     SelenideElement birthDateField = $("#dateOfBirthInput");
+    SelenideElement calendar = $(".react-datepicker");
     SelenideElement subjectField = $("#subjectsInput");
+    SelenideElement subjectMenu = $(".subjects-auto-complete__menu");
+    SelenideElement hobbyOptions = $("#hobbiesWrapper");
     SelenideElement uploadPictureButton = $("#uploadPicture");
     SelenideElement addressField = $("#currentAddress");
-    SelenideElement stateSelector = $x("//div[@id='state']//input");
-    SelenideElement citySelector = $x("//div[@id='city']//input");
+    SelenideElement stateSelector = $("#state");
+    SelenideElement citySelector = $("#city");
     SelenideElement submitButton = $("#submit");
     SelenideElement submittedFormContainer = $(".modal-content");
 
@@ -30,29 +33,16 @@ public class FillingFormTest extends TestBase {
     String email = "test@test.ru";
     String gender = "Male";
     String mobileNumber = "1234567891";
-    String birthDate = "1 December,1980";
-    String [] subjects = {"Maths", "English"};
-    String [] hobbies = {"Sports", "Reading"};
+    String yearOfBirth = "1980";
+    String monthOfBirth = "May";
+    String dayOfBirth = "28";
+    String fullBirthDate = dayOfBirth + " " + monthOfBirth + "," + yearOfBirth;
+    ArrayList<String > subjects = new ArrayList<>(Arrays.asList("Maths", "English"));
+    ArrayList<String> hobbies = new ArrayList<>(Arrays.asList("Sports", "Reading"));
     String pictureName = picture.getName();
     String address = "Some city, another street";
     String state = "Haryana";
     String city = "Panipat";
-
-    void selectGender(String gender) {
-        $x(String.format("//input[@value='%s']//following-sibling::label", gender)).click();
-    }
-
-    void selectHobbies(String [] hobbyArr) {
-        for (String hobby : hobbyArr) {
-            $("#hobbiesWrapper").$(byText(hobby)).click();
-        }
-    }
-
-    void selectSubject(String [] subjectArr) {
-        for (String subject : subjectArr) {
-            subjectField.setValue(subject).pressEnter();
-        }
-    }
 
     void checkFormValue(String label, String value) {
         submittedFormContainer
@@ -66,17 +56,24 @@ public class FillingFormTest extends TestBase {
         firstNameField.setValue(firstName);
         lastNameField.setValue(lastName);
         emailField.setValue(email);
-        selectGender(gender);
+        genderOptions.$(byText(gender)).click();
         mobileNumberField.setValue(mobileNumber);
-        selectSubject(subjects);
-        selectHobbies(hobbies);
+        subjectField.setValue(subjects.get(0));
+        subjectMenu.$(byText(subjects.get(0))).click();
+        subjectField.setValue(subjects.get(1));
+        subjectMenu.$(byText(subjects.get(1))).click();
+        hobbyOptions.$(byText(hobbies.get(0))).click();
+        hobbyOptions.$(byText(hobbies.get(1))).click();
         birthDateField.click();
-        DatePicker picker = new DatePicker();
-        picker.selectDate(birthDate);
+        calendar.$(".react-datepicker__year-select").selectOption(yearOfBirth);
+        calendar.$(".react-datepicker__month-select").selectOption(monthOfBirth);
+        calendar.$(":not(.react-datepicker__day--outside-month).react-datepicker__day--0" + dayOfBirth).click();
         uploadPictureButton.uploadFile(picture);
         addressField.setValue(address);
-        stateSelector.setValue(state).pressEnter();
-        citySelector.setValue(city).pressEnter();
+        stateSelector.scrollIntoView(true).click();
+        stateSelector.$(byText(state)).click();
+        citySelector.click();
+        citySelector.$(byText(city)).click();
         submitButton.click();
 
         submittedFormContainer.shouldBe(Condition.visible);
@@ -84,9 +81,9 @@ public class FillingFormTest extends TestBase {
         checkFormValue("Student Email", email);
         checkFormValue("Gender", gender);
         checkFormValue("Mobile", mobileNumber);
-        checkFormValue("Date of Birth", birthDate);
-        checkFormValue("Subjects", Arrays.toString(subjects).replaceFirst("\\[", "").replaceFirst("]", ""));
-        checkFormValue("Hobbies", Arrays.toString(hobbies).replaceFirst("\\[", "").replaceFirst("]", ""));
+        checkFormValue("Date of Birth", fullBirthDate);
+        checkFormValue("Subjects", subjects.get(0) + ", " + subjects.get(1));
+        checkFormValue("Hobbies", hobbies.get(0) + ", " + hobbies.get(1));
         checkFormValue("Picture", pictureName);
         checkFormValue("Address", address);
         checkFormValue("State and City", state + " " + city);
